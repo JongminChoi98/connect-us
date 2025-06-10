56e8134bdf44c0df7ca5f7bf92113400b7bacf0b
Jongmin Choi <138505370+JongminChoi98@users.noreply.github.com>
2025-06-10 22:40:10 +0900

    [ADD] auth pages

---
+++ app/features/auth/pages/social-complete-page.tsx
@@ -0,0 +1,9 @@
+import { Route } from "./+types/social-complete-page";
+
+export const meta: Route.MetaFunction = () => {
+  return [{ title: "Complete Social Login | connectus" }];
+};
+
+export default function SocialCompletePage() {
+  return <div></div>;
+}
---
+++ app/features/auth/pages/social-start-page.tsx
@@ -0,0 +1,9 @@
+import { Route } from "./+types/social-start-page";
+
+export const meta: Route.MetaFunction = () => {
+  return [{ title: "Social Login | connectus" }];
+};
+
+export default function SocialStartPage() {
+  return <div></div>;
+}
