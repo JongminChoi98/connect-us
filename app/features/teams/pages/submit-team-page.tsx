321052883e890a85cb47a3dbce102765123c5f11
Jongmin Choi <138505370+JongminChoi98@users.noreply.github.com>
2025-06-15 11:25:43 +0900

    [ADD] ideas, community, teams, message table migration

--- app/features/teams/pages/submit-team-page.tsx
+++ app/features/teams/pages/submit-team-page.tsx
@@ -3,10 +3,11 @@ import { Hero } from "~/common/components/hero";
 import InputPair from "~/common/components/input-pair";
 import SelectPair from "~/common/components/select-pair";
 import { Button } from "~/common/components/ui/button";
+import { PRODUCT_STAGES } from "../constants";
 import { Route } from "./+types/submit-team-page";
 
 export const meta: Route.MetaFunction = () => [
-  { title: "Create Team | connectus" },
+  { title: "Create Team | wemake" },
 ];
 
 export default function SubmitTeamPage() {
@@ -31,12 +32,7 @@ export default function SubmitTeamPage() {
             name="stage"
             required
             placeholder="Select the stage of your product"
-            options={[
-              { label: "Idea", value: "idea" },
-              { label: "Prototype", value: "prototype" },
-              { label: "MVP", value: "mvp" },
-              { label: "Product", value: "product" },
-            ]}
+            options={PRODUCT_STAGES}
           />
           <InputPair
             label="What is the size of your team?"
